641b4b37b205549c369386b96921d3377dc607db
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-03-19 17:55:52 -0300

    Update codeMapaStreamlit.py

--- codeMapaStreamlit.py
+++ codeMapaStreamlit.py
@@ -6,8 +6,8 @@ import zipfile
 import io
 
 # URLs dos arquivos shapefile no GitHub (com permalinks)
-bandeirantes_url = 'https://github.com/TheLastEnvoy/mapaStreamlit/blob/81cccc596276398955d324e9d8f5a28f2eafb6f9/shpsPerimetro.zip'
-perimetro_url = 'https://github.com/TheLastEnvoy/mapaStreamlit/blob/635b3f171d80097385127ca7e9bffae0e0469580/shpsBandeirantes.zip'
+bandeirantes_url = 'https://github.com/TheLastEnvoy/mapaStreamlit/raw/81cccc596276398955d324e9d8f5a28f2eafb6f9/shpsPerimetro.zip'
+perimetro_url = 'https://github.com/TheLastEnvoy/mapaStreamlit/raw/635b3f171d80097385127ca7e9bffae0e0469580/shpsBandeirantes.zip'
 
 # Função para carregar e exibir o mapa a partir de um arquivo shapefile
 def show_map(shapefile_url):
@@ -17,7 +17,7 @@ def show_map(shapefile_url):
         zip_ref.extractall('temp')
 
     # Carrega o arquivo shapefile
-    gdf = gpd.read_file('temp/bandeirantesDashboard.shp')
+    gdf = gpd.read_file('temp/shpsPerimetro.shp')
 
     # Cria um mapa básico com o Folium
     m = folium.Map(location=[-25.4284, -49.2733], zoom_start=12)
