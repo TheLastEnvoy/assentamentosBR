1982baf17400bf3ce680a210e38abf89e419c968
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-07-01 17:03:13 -0300

    Update dashboardPAsBR.py

--- dashboardPAsBR.py
+++ dashboardPAsBR.py
@@ -46,8 +46,8 @@ if gdf is not None:
     st.markdown("(As informações exibidas neste site são públicas e estão disponíveis no [Portal de Dados Abertos](https://dados.gov.br/dados/conjuntos-dados/sistema-de-informacoes-de-projetos-de-reforma-agraria---sipra))")
     st.write("Contato: 6dsvj@pm.me")
 
-# Opções de basemap com atribuições corretas
-basemaps = {
+    # Opções de basemap com atribuições corretas
+    basemaps = {
     'OpenStreetMap': folium.TileLayer('openstreetmap'),
     'Stamen Terrain': folium.TileLayer('stamenterrain', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'),
     'Stamen Toner': folium.TileLayer('stamentoner', attr='Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'),
